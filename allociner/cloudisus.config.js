08508279bee6f4bef88fd3c821d3350520ce081c
mhausenblas <michael.hausenblas@deri.org>
2012-01-15 11:32:18 +0000

    auth issues

--- allociner/cloudisus.config.js
+++ allociner/cloudisus.config.js
@@ -8,5 +8,5 @@ SERVER_HOSTNAME = "127.0.0.1"; // change this to the hostname where you deploy i
 SERVER_PORT = "8086"; // change this to the hostname where you deploy it (if not run locally)
 
 DATASTORE_ENDPOINT = "http://dydra.com/mhausenblas/sandbox/sparql?"; // change to the SPARQL endpoint where the data is hosted
-DATASTORE_USERNAME = "pJEuMyyLgNx75ntTVEWL"; // chage this to the API key from http://dydra.com/ -> 'My Account' -> 'Your API Key' 
+DATASTORE_USERNAME = ""; // chage this to the API key from http://dydra.com/ -> 'My Account' -> 'Your API Key' 
 DATASTORE_PASSWORD = "X"; // dummy, ingore
