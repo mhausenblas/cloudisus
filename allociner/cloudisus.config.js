0dff0e4e01874c4b3d3ae03700f92a3e264cce70
mhausenblas <michael.hausenblas@deri.org>
2012-01-16 06:40:02 +0000

    make Nodester compliant

--- allociner/cloudisus.config.js
+++ allociner/cloudisus.config.js
@@ -4,9 +4,9 @@
 //
 // Michael Hausenblas, 2012
 
-SERVER_HOSTNAME = "127.0.0.1"; // change this to the hostname where you deploy it (if not run locally)
-SERVER_PORT = "8086"; // change this to the hostname where you deploy it (if not run locally)
+SERVER_HOSTNAME = "cloudisus.nodester.com"; // change this to the hostname where you deploy it (if not run locally)
+SERVER_PORT = "13708"; // change this to the hostname where you deploy it (if not run locally)
 
 DATASTORE_ENDPOINT = "http://dydra.com/mhausenblas/sandbox/sparql?"; // change to the SPARQL endpoint where the data is hosted
-DATASTORE_USERNAME = ""; // chage this to the API key from http://dydra.com/ -> 'My Account' -> 'Your API Key' 
+DATASTORE_USERNAME = "pJEuMyyLgNx75ntTVEWL"; // chage this to the API key from http://dydra.com/ -> 'My Account' -> 'Your API Key' 
 DATASTORE_PASSWORD = "X"; // dummy, ingore
